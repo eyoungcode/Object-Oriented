290302baa5744364b619eff01df024fbc15a4c4a
Erik Young <youngblkraven@gmail.com>
2019-04-22 21:35:42 -0600

    added classes and doc

--- php/classes/foo.php
+++ php/classes/foo.php
@@ -1,1 +1,29 @@
 <?php
+
+class author {
+	/**
+	 * id for the author, this is the primary key
+	 **/
+private $authorId;
+	/**
+ * avartar of the author
+ **/
+	private $avatarUrl;
+	/**
+	 * activationToken the author
+	 **/
+	private $activationToken;
+	/**
+	 * email of the author
+	 **/
+	private $email;
+	/**
+		 hash of the author
+	 **/
+	private $hash;
+	/**
+	 * userName of the author
+	 **/
+	private $userName;
+};
+
